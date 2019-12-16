bb44e74eb8d8e4ad216c3b01342b633b24cf34cc
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2019-12-15 20:56:17 -0600

    functioning app, besides minor bug with first note in arary

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 let notes = [];
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Home page
 app.get("/", function (req, res) {
@@ -25,38 +27,72 @@ app.get("/api/notes", function (req, res) {
 });
 
 //saves the information on the page to the db.json file (still working on this)
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
     jsonFile = path.join(__dirname, "../../../db/db.json");
-    newNote = req;
-    console.log(newNote);
+    newNote = req.body;
 
     //gets the JSON file and saves it to the "notes variable"
-   function getJsonFile(){
-       fs.readFile(jsonFile, "utf8", function(error, response) {
-           if (error) {
-               console.log(error);
+    function getJsonFile() {
+        fs.readFile(jsonFile, "utf8", function (error, response) {
+            if (error) {
+                console.log(error);
             }
             notes = JSON.parse(response)
-            // notes.push(newNote)
             writeJsonFile();
         });
     } getJsonFile()
 
     //re-writes the db.json file with the notes variable
-    function writeJsonFile(){
-        fs.writeFile(jsonFile, JSON.stringify(notes), function(err) {
+    function writeJsonFile() {
+        notes.push(newNote)
+        for (let i = 0; i < notes.length; i++) {
+            note = notes[i]
+            note.id = i
+        }
+        fs.writeFile(jsonFile, JSON.stringify(notes), function (err) {
 
             if (err) {
-              return console.log(err);
+                return console.log(err);
             }
-          
-            console.log("Success!");
-          
-          });
-          
+            console.log("ADDED!");
+        });
+
     }
+    res.sendFile(path.join(__dirname, "../../../db/db.json"));
+
 });
 
+//Deletes a note when that note is clicked on
+app.delete("/api/notes/:id", function (req, res) {
+
+    id = (req.params.id)
+
+    jsonFile = path.join(__dirname, "../../../db/db.json");
+    function getJsonFile() {
+        fs.readFile(jsonFile, "utf8", function (error, response) {
+            if (error) {
+                console.log(error);
+            }
+            notes = JSON.parse(response)
+            deleteJsonFile();
+        });
+    } getJsonFile()
+
+    function deleteJsonFile() {
+        notes.splice(id, 1);
+        writeFile();
+    }
+
+    function writeFile() {
+        fs.writeFile(jsonFile, JSON.stringify(notes), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("DELETED!");
+        });
+    }
+    res.sendFile(path.join(__dirname, "../../../db/db.json"));
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
